fa99c9ca48904416aebf494524325a0cc9dcd00b
Juniza Permata Nugraheni <juniza.permata@students.utdi.ac.id>
2022-06-03 20:41:47 +0700

    coba

--- src/pages/LoginScreen/index.js
+++ src/pages/LoginScreen/index.js
@@ -1,10 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity} from 'react-native'
 import React from 'react'
 
 const LoginScreen = () => {
   return (
     <View>
       <Text>LoginScreen</Text>
+      <TouchableOpacity>Button</TouchableOpacity>
     </View>
   )
 }
